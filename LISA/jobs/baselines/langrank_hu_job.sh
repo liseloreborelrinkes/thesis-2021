e0186e2aebefe633a6b088fa64f66d584b16f411
liseloreborelrinkes <43146037+liseloreborelrinkes@users.noreply.github.com>
2021-06-27 15:56:29 +0200

    Add files via upload

---
+++ LISA/jobs/baselines/langrank_hu_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=06:00:00
+#SBATCH --partition gpu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=liseloreborelrinkes@hotmail.com
+
+#Loading modules
+module load 2020
+module load Python
+module load CUDA/11.0.2-GCC-9.3.0
+
+#Execute a Python program located in $HOME.
+python $HOME/OpenNMT-py/train.py -config $HOME/config/langrank_hu_train.yaml
