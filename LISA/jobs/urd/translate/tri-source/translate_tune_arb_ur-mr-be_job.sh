b40f6905cca274922c33168bc36901c6537dfcb9
liseloreborelrinkes <43146037+liseloreborelrinkes@users.noreply.github.com>
2021-06-27 15:50:29 +0200

    Add files via upload

---
+++ LISA/jobs/urd/translate/tri-source/translate_tune_arb_ur-mr-be_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=00:10:00
+#SBATCH --partition gpu_short
+
+#Loading modules
+module load 2020
+module load Python
+module load CUDA/11.0.2-GCC-9.3.0
+
+#Execute a Python program located in $HOME.
+python $HOME/OpenNMT-py/translate.py -model $HOME/saved_models/final_output/tune/urd/ur-mr-be_arb_model_step_201000.pt -src $HOME/bpe_data/final_data/test_data/final_ur_test_bpe.txt -output $HOME/bpe_data/final_data/pred_data/pred_tune_arb_ur_mr_be_test_201000.txt -verbose -replace_unk -gpu 0
+
+sed -i "s/@@ //g"  $HOME/bpe_data/final_data/pred_data/pred_tune_arb_ur_mr_be_test_201000.txt
+
+perl  $HOME/OpenNMT-py/tools/multi-bleu.perl $HOME/bpe_data/final_data/test_data/ur_en_test.txt < $HOME/bpe_data/final_data/pred_data/pred_tune_arb_ur_mr_be_test_201000.txt
