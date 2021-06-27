8af3e64f7f45ea807cc38c53304904ef6b2be574
liseloreborelrinkes <43146037+liseloreborelrinkes@users.noreply.github.com>
2021-06-27 15:37:05 +0200

    Add files via upload

---
+++ LISA/jobs/lit/tune/bi-source/tune_arb_lt-kk_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=00:15:00
+#SBATCH --partition gpu_short
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=liseloreborelrinkes@hotmail.com
+
+#Loading modules
+module load 2020
+module load Python
+module load CUDA/11.0.2-GCC-9.3.0
+
+#Execute a Python program located in $HOME.
+python $HOME/OpenNMT-py/train.py -train_from $HOME/saved_models/final_output/arb_model__step_200000.pt -config $HOME/config/final_arb_lt-kk_tune.yaml
---
+++ LISA/jobs/lit/tune/bi-source/tune_arb_lt-ko_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=02:00:00
+#SBATCH --partition gpu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=liseloreborelrinkes@hotmail.com
+
+#Loading modules
+module load 2020
+module load Python
+module load CUDA/11.0.2-GCC-9.3.0
+
+#Execute a Python program located in $HOME.
+python $HOME/OpenNMT-py/train.py -train_from $HOME/saved_models/final_output/arb_model__step_200000.pt -config $HOME/config/final_arb_lt-ko_tune.yaml
